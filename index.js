995693c3d06f9054f03278f9185c329212200164
Bryan Jay Zacarias <zacarbr@oocl.com>
2022-08-18 15:36:30 +0800

    ZACARBR ARRAy

--- index.js
+++ index.js
@@ -3,24 +3,74 @@ var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
 
+console.log(Array.isArray(a));
+console.log(Array.isArray(b));
+
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
 
+for(var x = 0; x < a.length; x++){
+    a[x] = a[x] * 2;
+}
+
+console.log(a); // [ 2, 4, 6, 8, 10 ]
+console.log(a.toString()); // 2,4,6,8,10
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
+var case1 = '';
+for (var j = 0; j < colors.length; j++){
+    case1 = case1 + " " + colors[j];
+}
+
+console.log(case1);
+
+
 // case 2 output: 'Red+Green+White+Black'
+var case2 = colors[0];
+for (var j = 1; j < colors.length; j++){
+    case2 = case2 + "+" + colors[j];
+}
+
+console.log(case2);
+
 // case 3 output: 'Red,Green,White,Black'
+var case3 = colors[0];
+for (var j = 1; j < colors.length; j++){
+    case3 = case3 + "," + colors[j];
+}
+
+console.log(case3);
+
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+a.sort((a,b)=>b-a)
+console.log(a);
 
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+var b = 1;
+var c = 0;
+var item;
+    for (var i=0; i<a.length; i++){
+        for (var j=i; j<a.length; j++)
+        {
+                if (a[i] == a[j])
+                 c++;
+                if (b<c)
+                {
+                  b=c;
+                  item = a[i];
+                }
+        }
+        c=0;
+    }
+console.log(item);
